6bb69d8d978e6bc7f77c6faea5b825ede01b46b5
Lawrence O. Caulker <locaulker@gmail.com>
2021-01-21 22:30:12 -0500

    Started L107

--- 5-menu/setup/src/Categories.js
+++ 5-menu/setup/src/Categories.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React from 'react'
 
-const Categories = () => {
-  return <h2>categories component</h2>;
-};
+const Categories = ({ filteredItems }) => {
+  return (
+    <div className='btn-container'>
+      <button className='filter-btn' onClick={() => filteredItems('all')}>
+        All
+      </button>
+      <button className='filter-btn' onClick={() => filteredItems('breakfast')}>
+        Breakfast
+      </button>
+    </div>
+  )
+}
 
-export default Categories;
+export default Categories
