eac2826f518af72613313dffc8c7f49fb3df4363
Lawrence <locaulker@gmail.com>
2021-01-26 06:16:13 -0500

    Added Sidebar and Modal structure

--- 12-sidebar-modal/setup/src/data.js
+++ 12-sidebar-modal/setup/src/data.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import {
   FaBehance,
   FaFacebook,
@@ -9,65 +9,66 @@ import {
   FaUserFriends,
   FaFolderOpen,
   FaCalendarAlt,
-  FaWpforms,
-} from 'react-icons/fa';
+  FaWpforms
+} from 'react-icons/fa'
+
 export const links = [
   {
     id: 1,
     url: '/',
     text: 'home',
-    icon: <FaHome />,
+    icon: <FaHome />
   },
   {
     id: 2,
     url: '/team',
     text: 'team',
-    icon: <FaUserFriends />,
+    icon: <FaUserFriends />
   },
   {
     id: 3,
     url: '/projects',
     text: 'projects',
-    icon: <FaFolderOpen />,
+    icon: <FaFolderOpen />
   },
   {
     id: 4,
     url: '/calendar',
     text: 'calendar',
-    icon: <FaCalendarAlt />,
+    icon: <FaCalendarAlt />
   },
   {
     id: 5,
     url: '/documents',
     text: 'documents',
-    icon: <FaWpforms />,
-  },
-];
+    icon: <FaWpforms />
+  }
+]
 
 export const social = [
   {
     id: 1,
     url: 'https://www.twitter.com',
-    icon: <FaFacebook />,
+    icon: <FaFacebook />
   },
   {
     id: 2,
     url: 'https://www.twitter.com',
-    icon: <FaTwitter />,
+    icon: <FaTwitter />
   },
   {
     id: 3,
     url: 'https://www.twitter.com',
-    icon: <FaLinkedin />,
+    icon: <FaLinkedin />
   },
   {
     id: 4,
     url: 'https://www.twitter.com',
-    icon: <FaBehance />,
+    icon: <FaBehance />
   },
   {
     id: 5,
     url: 'https://www.twitter.com',
-    icon: <FaSketch />,
-  },
-];
+    icon: <FaSketch />
+  }
+]
