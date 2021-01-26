429a07d35bfa1096982d73370149c4beda99a194
Lawrence O. Caulker <locaulker@gmail.com>
2021-01-26 09:24:54 -0500

    Completed sidebar functionality

--- 12-sidebar-modal/setup/src/Home.js
+++ 12-sidebar-modal/setup/src/Home.js
@@ -1,16 +1,18 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import { FaBars } from 'react-icons/fa'
-import { AppContext } from './context'
+import { useGlobalContext } from './context'
 
 const Home = () => {
-  const data = useContext(AppContext)
-  console.log(data)
+  const { openSidebar, openModal } = useGlobalContext()
+
   return (
     <main>
-      <button className='sidebar-toggle'>
+      <button className='sidebar-toggle' onClick={openSidebar}>
         <FaBars />
       </button>
-      <button className='btn'>Show Modal</button>
+      <button className='btn' onClick={openModal}>
+        Show Modal
+      </button>
     </main>
   )
 }
