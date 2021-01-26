f9c96bf358cebc5db04f267a588312393c12a2df
Lawrence O. Caulker <locaulker@gmail.com>
2021-01-25 22:24:38 -0500

    Start at L138

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -4,7 +4,43 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  return <h4>navbar</h4>
+  return (
+    <nav>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} alt='Site Logo' />
+          <button className='nav-toggle'>
+            <FaBars />
+          </button>
+        </div>
+        <div className='links-container show-container'>
+          <ul className='links'>
+            {links.map(link => {
+              const { id, url, text } = link
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+        <ul className='social-icons'>
+          {social.map(socialIcon => {
+            const { id, url, icon } = socialIcon
+            return (
+              <li key={id}>
+                <a href={url} target='_blank'>
+                  {icon}
+                </a>
+              </li>
+            )
+          })}
+        </ul>
+        <ul className='social-icons'></ul>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
